dc8ba4ba4e4712d38848dee571968d936a73157b
Fatih Acet <acetfatih@gmail.com>
2017-01-07 22:47:43 +0300

    Add v-for

---
+++ 02-v-for/app.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', () => {
+
+  window.vue = new Vue({
+    el: '#app',
+    name: 'Cart',
+    data: {
+      isLoading: true,
+      cart: [],
+      saved: []
+    },
+    methods: {
+      removeFromCart(index, fromCart) {
+        this.cart.splice(index, 1);
+      },
+      saveForLater(index) {
+        const item = this.cart.splice(index, 1);
+        this.saved.push(item[0]);
+      },
+      removeFromSavedList(index) {
+        this.saved.splice(index);
+      },
+      moveToCart(index) {
+        const item = this.saved.splice(index, 1);
+        this.cart.push(item[0]);
+      }
+    },
+    created() {
+      fetch('./data.json')
+        .then((res) => { return res.json() })
+        .then((res) => {
+          this.isLoading = false;
+          this.cart = res.cart;
+          this.saved = res.saved;
+        })
+    }
+  })
+
+
+});
