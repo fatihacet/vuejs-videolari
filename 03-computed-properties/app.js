62c82f0fc31cea5c8c3c292de926a8968afe5241
Fatih Acet <acetfatih@gmail.com>
2017-01-08 00:41:13 +0300

    Add computed properties

---
+++ 03-computed-properties/app.js
@@ -0,0 +1,47 @@
+window.addEventListener('load', () => {
+
+  window.vue = new Vue({
+    el: '#app',
+    name: 'Cart',
+    data: {
+      isLoading: true,
+      cart: [],
+      saved: []
+    },
+    methods: {
+      removeFromCart(index, fromCart) {
+        this.cart.splice(index, 1);
+      },
+      saveForLater(index) {
+        const item = this.cart.splice(index, 1);
+        this.saved.push(item[0]);
+      },
+      removeFromSavedList(index) {
+        this.saved.splice(index);
+      },
+      moveToCart(index) {
+        const item = this.saved.splice(index, 1);
+        this.cart.push(item[0]);
+      }
+    },
+    computed: {
+      cartTotal() {
+        let total = 0;
+        this.cart.forEach((item) => {
+          total += parseFloat(item.price, 10);
+        });
+        return total.toFixed(2);
+      }
+    },
+    created() {
+      fetch('./data.json')
+        .then((res) => { return res.json() })
+        .then((res) => {
+          this.isLoading = false;
+          this.cart = res.cart;
+          this.saved = res.saved;
+        })
+    }
+  })
+
+});
